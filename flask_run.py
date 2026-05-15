7b7fcddce8b8454088f3d2ee83e3c7334c80f747
Ryan Shuler <ryaneshuler@gmail.com>
2026-05-15 09:36:36 -0400

    Fix config name for production

--- flask_run.py
+++ flask_run.py
@@ -1,6 +1,6 @@
 from app import create_app
 
-app = create_app('DevelopmentConfig, ProductionConfig')
+app = create_app('ProductionConfig')
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
