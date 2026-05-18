4845a8e742f1c60bc66cd2cd35d6c2b1cd878e28
Ryan Shuler <ryaneshuler@gmail.com>
2026-05-18 13:21:30 -0400

    New fix to extensions for GitHub Actions

--- app/extensions.py
+++ app/extensions.py
@@ -5,6 +5,7 @@ from flask_limiter.util import get_remote_address
 from flask_caching import Cache
 
 db = SQLAlchemy()
+Base = db.Model
 ma = Marshmallow()
 
 limiter = Limiter(key_func=get_remote_address)
